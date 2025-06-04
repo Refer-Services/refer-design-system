40bfc31b5fc15e99e56308c51fda3ff2d13ab718
dudufigueiredo <dudufigueiredo@gmail.com>
2025-06-04 10:26:33 -0300

    feat: adjust button outline background

--- registry/refer/ui/button.tsx
+++ registry/refer/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/75 border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px] focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/80",
         outline:
-          "border border-input bg-card shadow-xs hover:bg-accent hover:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground",
+          "border border-input bg-card shadow-xs hover:bg-background dark:hover:bg-accent hover:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/90 border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px]",
         ghost:
