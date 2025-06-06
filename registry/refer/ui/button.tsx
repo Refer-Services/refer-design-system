f426bd2a786e44c05f99fa10591cf636ad20a52c
dudufigueiredo <dudufigueiredo@gmail.com>
2025-06-06 15:49:27 -0300

    feat: add button active states

--- registry/refer/ui/button.tsx
+++ registry/refer/ui/button.tsx
@@ -5,22 +5,22 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-1.5 whitespace-nowrap rounded-full text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer active:scale-pressed active:opacity-90 duration-0 ease-out",
+  "inline-flex items-center justify-center gap-1.5 whitespace-nowrap rounded-full text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer active:scale-pressed duration-0",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px]",
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 active:bg-primary border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px]",
         destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/75 border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px] focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/80",
+          "bg-destructive text-white shadow-xs hover:bg-destructive/90 active:bg-destructive dark:bg-destructive/80 dark:hover:bg-destructive/70 border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px] focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
-          "border border-input bg-card shadow-xs hover:bg-background dark:hover:bg-accent hover:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground",
+          "border border-input bg-card shadow-xs hover:bg-background active:bg-stone-100  dark:hover:bg-accent dark:active:bg-accent/60 hover:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/90 border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px]",
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/90 active:bg-secondary border-black/20 border-t-[0.5px] border-x-[0.5px] border-b-[2px]",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent [&_svg:not([class*='text-'])]:text-muted-foreground",
+          "hover:bg-accent hover:text-accent-foreground active:bg-stone-200/50 dark:hover:bg-accent dark:active:bg-accent/60 [&_svg:not([class*='text-'])]:text-muted-foreground",
         subtle:
-          "bg-muted hover:bg-border/70 [&_svg:not([class*='text-'])]:text-muted-foreground",
+          "bg-muted hover:bg-border/70 active:bg-stone-200/80 dark:active:bg-border/90 [&_svg:not([class*='text-'])]:text-muted-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
